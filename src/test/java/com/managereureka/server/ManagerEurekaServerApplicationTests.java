d605a9ec7205f5a253b6916d959af71a3f304a8f
hclee <hoike@naver.com>
2023-11-22 20:41:40 +0900

    application.yml

---
+++ src/test/java/com/managereureka/server/ManagerEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.managereureka.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagerEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
